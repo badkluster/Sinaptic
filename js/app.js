9ee25406efb560cafc1e6feaadb3746067039980
badkluster <cabronator_pesuti@hotmail.com>
2021-07-25 09:36:32 -0300

    update

--- js/app.js
+++ js/app.js
@@ -7,15 +7,18 @@ var btnMenuOpen = document.getElementById("btnMenuOpen"),
 // Click abrir
 btnMenuOpen.addEventListener("click", function () {
   menuResponsive.classList.add("active");
+  document.body.classList.add("hidden");
 });
 
 // Click cerrar
 btnMenuClose.addEventListener("click", function () {
   menuResponsive.classList.remove("active");
+  document.body.classList.remove("hidden");
 });
 
 // Cerrar menu con elementos de enlace
 enlaces.addEventListener("click", function () {
+  document.body.classList.remove("hidden");
   menuResponsive.style.transitionDelay = "0.5s";
   menuResponsive.classList.remove("active");
 });
@@ -86,7 +89,7 @@ function agregarImagen() {
 
   const imagenClientes = document.createElement("IMG");
   imagenClientes.src = "./assets/bannerclientescolor.webp";
-  imagenClientes.style = "max-width: 100%; width: 100%; height: auto;";
+  imagenClientes.style = "max-width: 100%; width:100%; height: auto";
   imagenClientes.alt = "imagen Clientes";
   document.querySelector("#clientesImg").appendChild(imagenClientes);
 }
@@ -105,3 +108,59 @@ function agregarImagen() {
 //     parrafo.map((p) => p.classList.replace("visible", "invisible"));
 //   });
 // });
+
+(function () // Code in a function to create an isolate scope
+{
+  var speed = 1500;
+  var moving_frequency = 15; // Affects performance !
+  var links = document.getElementsByTagName("a");
+  var href;
+  for (var i = 0; i < links.length; i++) {
+    href =
+      links[i].attributes.href === undefined
+        ? null
+        : links[i].attributes.href.nodeValue.toString();
+    if (href !== null && href.length > 1 && href.substr(0, 1) == "#") {
+      links[i].onclick = function () {
+        var element;
+        var href = this.attributes.href.nodeValue.toString();
+        if ((element = document.getElementById(href.substr(1)))) {
+          var hop_count = speed / moving_frequency;
+          var getScrollTopDocumentAtBegin = getScrollTopDocument();
+          var gap =
+            (getScrollTopElement(element) - getScrollTopDocumentAtBegin) /
+            hop_count;
+
+          for (var i = 1; i <= hop_count; i++) {
+            (function () {
+              var hop_top_position = gap * i;
+              setTimeout(function () {
+                window.scrollTo(
+                  0,
+                  hop_top_position + getScrollTopDocumentAtBegin
+                );
+              }, moving_frequency * i);
+            })();
+          }
+        }
+
+        return false;
+      };
+    }
+  }
+
+  var getScrollTopElement = function (e) {
+    var top = 0;
+
+    while (e.offsetParent != undefined && e.offsetParent != null) {
+      top += e.offsetTop + (e.clientTop != null ? e.clientTop : 0);
+      e = e.offsetParent;
+    }
+
+    return top;
+  };
+
+  var getScrollTopDocument = function () {
+    return document.documentElement.scrollTop + document.body.scrollTop;
+  };
+})();
